85f2430935e0bd2c846a88c543d4aa8a8120c208
starshorse <star_horse@naver.com>
2021-05-08 08:46:40 +0900

    angular-form

---
+++ angular-form/app/dataEz/ng-json-form.js
@@ -0,0 +1,12 @@
+var dataEz =
+[
+{"id":"Id","title":"Id","type":"string","pattern":"","maxLength":"","description":"","validationMessage":""},
+{"id":"Password","title":"Password","type":"string","pattern":"","maxLength":"","description":"","validationMessage":""},
+{"id":"PasswordConfirm","title":"Password Confirm","type":"string","pattern":"","maxLength":"","description":"","validationMessage":""},
+{"id":"Lname","title":"Last Name","type":"string","pattern":"","maxLength":"","description":"","validationMessage":""},
+{"id":"Fname","title":"First Name","type":"string","pattern":"","maxLength":"","description":"","validationMessage":""},
+{"id":"CompanyName","title":"Company Name","type":"string","pattern":"","maxLength":"","description":"","validationMessage":""},
+{"id":"Email","title":"Email","type":"string","pattern":"^\\\\S+@\\\\S+$","maxLength":"20","description":"Email will be used for evil.","validationMessage":""},
+{"id":"Comment","title":"Comment","type":"string","pattern":"","maxLength":"","description":"","validationMessage":"Don��t be greedy!"}
+
+]
