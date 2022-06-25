bcf68e4b65846c762df3f2884cdffcdeb2a11125
starshorse <star_horse@naver.com>
2022-06-25 11:06:00 +0900

    averatec: mongodb create google ID. mongodb+srv://rrr:name@cluster0.9fnmklj.mongodb.net/?retryWrites=true&w=majority

--- server/app.js
+++ server/app.js
@@ -21,5 +21,23 @@ server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
+// 몽구스 연결
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://rrr:ch1whdrb@cluster0.9fnmklj.mongodb.net/?retryWrites=true&w=majority',	  
+//'mongodb+srv://devCecy:<password>@devcecy.dprgf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+      // useNewUrlPaser: true,
+      // useUnifiedTofology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    }
+  )
+  .then(() => console.log('MongoDB conected'))
+  .catch((err) => {
+    console.log(err);
+  });
+
 // Expose app
 exports = module.exports = app;
