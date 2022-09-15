db0ebc4d927accd271cbbc72c5609e1460fadffa
DESKTOP-JC987LD\user <star_horse@naver.com>
2022-09-15 10:32:34 +0900

    diary/Install/ezoffice_builder lib spreadjs 15

---
+++ diary/Install/ezoffice_builder/Bld_framework/codeTree/ezworks/app/lib/spreadjs/15/samples/VueSample/vue3/src/components/QuickStart.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="componentContainer" >
+    <h3>빠른 시작</h3>
+    <div>
+      Vue 응용 프로그램에서 스프레드시트를 시작하는 방법은 다음과 같습니다.
+      <div>
+        <p>1. Vue 응용 프로그램에 참조 파일을 추가합니다.</p>
+        <p>2. 데이터와 논리를 제공할 컴포넌트를 추가합니다.</p>
+        <p>3. 데이터를 스프레드의 몇 가지 기타 옵션과 바인딩합니다.</p>
+        <p>4. 몇 가지 CSS를 추가하여 모양을 사용자 정의합니다.</p>
+      </div>
+    </div>
+    <div class="spreadContainer" >
+      <gc-spread-sheets
+        :hostClass='"spreadHost"'>
+
+      </gc-spread-sheets>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+//    name: 'sample-header'
+  }
+</script>
+<style scoped>
+  .componentContainer {
+    position: absolute;
+    padding: 10px;
+    left: 242px;
+    top: 0;
+    bottom: 20px;
+    right: 0;
+  }
+  .spreadContainer {
+    padding: 10px;
+    box-shadow: 0 0 20px grey;
+  }
+  .spreadContainer{
+    position: absolute;
+    left: 0px;
+    right: 30px;
+    top: 260px;
+    bottom: 10px;
+  }
+  .spreadHost{
+    width: 100%;
+    height: 100%;
+  }
+
+
+</style>
---
+++ diary/Install/ezoffice_builder/Bld_framework/codeTree/ezworks/app/lib/spreadjs/15/samples/VueSample/vue3/src/components/WorkSheet.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="componentContainer gc-scrollbar-thin" >
+    <h3>GC-Worksheet</h3>
+    <p>아래 샘플은 시트의 일부 속성을 바인딩하는 방법을 보여줍니다.</p>
+
+    <div class="spreadContainer " >
+      <gc-spread-sheets
+        :hostClass='hostClass'
+
+      >
+        <gc-worksheet
+          :dataSource="dataSource"
+          :rowHeaderVisible = 'rowHeaderVisible'
+          :columnHeaderVisible = 'columnHeaderVisible'
+          :frozenRowCount = 'frozenRowCount'
+          :frozenColumnCount = 'frozenColumnCount'
+          :frozenTrailingRowCount = 'frozenTrailingRowCount'
+          :frozenTrailingColumnCount = 'frozenTrailingColumnCount'
+          :sheetTabColor = 'sheetTabColor'
+          :frozenlineColor = 'frozenlineColor'
+          :selectionBackColor = 'selectionBackColor'
+          :selectionBorderColor = 'selectionBorderColor'
+        >
+          <gc-column :width="'150'" :dataField="'name'"></gc-column>
+          <gc-column :width="'150'" :dataField="'phone'"></gc-column>
+          <gc-column :width="'150'" :dataField="'country'"></gc-column>
+          <gc-column :width="'150'" :dataField="'email'"></gc-column>
+          <gc-column :width="'150'" :dataField="'onJob'"></gc-column>
+        </gc-worksheet>
+
+      </gc-spread-sheets>
+    </div>
+    <div class="test-btn-list">
+        <label >
+          <input type="checkbox" :checked="rowHeaderVisible" @click="rowHeaderVisible = !rowHeaderVisible"/>행 머리글 표시
+        </label>
+        <label>
+          <input type="checkbox" :checked="columnHeaderVisible" @click="columnHeaderVisible = !columnHeaderVisible"/>열 머리글 표시
+        </label>
+      <br>
+        <label>
+          <input type="number" v-model="frozenRowCount" />고정된 행 수
+        </label>
+        <label>
+          <input type="number" v-model="frozenColumnCount" />고정된 열 수
+        </label>
+      <br>
+        <label>
+          <input type="number" v-model="frozenTrailingRowCount" />고정된 후행 행 수
+        </label>
+        <label>
+          <input type="number" v-model="frozenTrailingColumnCount" />고정된 후행 열 수
+        </label>
+      <br>
+        <label>
+          <input type="color"  v-bind:value="sheetTabColor" v-on:change="setSheetTabColor"/>시트 탭 색
+        </label>
+        <label>
+          <input type="color"  v-bind:value="frozenlineColor" v-on:change="setFrozenlineColor"/>고정된 선 색
+        </label>
+      <br>
+        <label>
+          <input type="color"  v-bind:value="selectionBorderColor" v-on:change="setSelectionBorderColor"/>선택 영역 배경색
+        </label>
+        <label>
+          <input type="color" v-bind:value="selectionBackColor"  v-on:change="setSelectionBackColor" rgba/>선택 영역 테두리 색
+        </label>
+
+    </div>
+
+  </div>
+</template>
+<script>
+  import DataService from '../static/dataService'
+
+  export default {
+    data(){
+      return {
+        hostClass:"spread-host",
+        dataSource :DataService.getEmployeesData(),
+        rowHeaderVisible :true,
+        columnHeaderVisible : true,
+        frozenRowCount: 2,
+        frozenColumnCount:3,
+        frozenTrailingRowCount:0,
+        frozenTrailingColumnCount:0,
+        sheetTabColor:"#61E6E6",
+        frozenlineColor:"#000000",
+        selectionBackColor:'#D0D0D0',
+        selectionBorderColor:'#217346'
+      }
+    },
+    methods:{
+      setSheetTabColor(e){
+        this.sheetTabColor = e.target.value;
+      },
+      setFrozenlineColor(e){
+        this.frozenlineColor = e.target.value;
+      },
+      setSelectionBackColor(e){
+        this.selectionBackColor = e.target.value;
+      },
+      setSelectionBorderColor(e){
+        this.selectionBorderColor = e.target.value;
+      }
+    }
+
+
+  }
+</script>
+<style scoped>
+  .componentContainer {
+    position: absolute;
+    padding: 10px;
+    left: 242px;
+    top: 0;
+    bottom: 20px;
+    right: 3px;
+    overflow-y:auto ;
+    overflow-x: hidden;
+  }
+  .spreadContainer{
+    position: absolute;
+    top:90px;
+    padding: 10px;
+    /*width: 100%;*/
+    /*height: 240px;*/
+    left: 10px;
+    right:10px;
+    bottom: 150px;
+    box-shadow: 0 0 20px grey;
+  }
+  .test-btn-list{
+    /*padding: 20px;*/
+    position: absolute;
+    bottom: 0px;
+    height:150px;
+  }
+  .test-btn-list label{
+    display: inline-flex;
+    margin: 3px 20px;
+    width: 300px;
+    line-height: 23px;
+  }
+  .spread-host{
+    width: 100%;
+    height: 100%;
+  }
+
+</style>
