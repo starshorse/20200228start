963e3d24b0786cd08e783b85c38f34216f674d25
starshorse <star_horse@naver.com>
2022-09-22 07:53:14 +0900

    .

--- xls_node/protractor/s_01/s_01.spec.js
+++ xls_node/protractor/s_01/s_01.spec.js
@@ -13,10 +13,24 @@ describe('s_01', function(){
 //		browser.pause()
 		let first = element.all(by.css('.items li')).first() 
 		expect( first.getText()).toBe('First')
-		let elem = element.all( by.css('.items li')).filter((elem, index)=>{ return elem.getText().then( (text)=>text =='Third')})	
-		console.log( elem[0])
+		let elems = element.all( by.css('.items li')).filter((elem, index)=>{ return elem.getText().then( (text)=>text =='Third')})	
+		let f_text = await first.getText()
+
+		console.log( f_text )
+		expect( elems.first().getText()).toBe('Third') 
+		let t_text = await elems.first().getText() 
+		console.log( t_text ) 
 
 		element( by.id('foo')).click()
+		let items = element( by.css(',items')).$$('li')
+//		expect( items.first().getText()).toBe('First')
+
+		expect($('.items').isPresent()).toBeTruthy()
+		
+		let s1 = await $$('.items li').first().getText()
+		console.log(s1) 
+		 
+		console.log( $('.items').locator() )
 
 	})
 /*	
