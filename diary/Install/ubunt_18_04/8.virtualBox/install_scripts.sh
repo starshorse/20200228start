4e5b2049438ec64aaf9efb11eb89113d01af3cc6
starshorse <star_horse@naver.com>
2022-09-26 11:51:15 +0900

    local virtualbox

--- diary/Install/ubunt_18_04/8.virtualBox/install_scripts.sh
+++ diary/Install/ubunt_18_04/8.virtualBox/install_scripts.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
 
 wget http://mirror.kakao.com/ubuntu-releases/16.04.7/ubuntu-16.04.6-desktop-i386.iso
+wget https://releases.ubuntu-mate.org/archived/18.04/ubuntu-mate-18.04.5-desktop-i386.iso
+
 sudo apt install virtualbox
