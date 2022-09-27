834ab62b0894937b23ef2c9dfe7f9083c63e54bc
starshorse <star_horse@naver.com>
2022-09-28 07:27:47 +0900

    home-DT Install/u16.04/python3

---
+++ diary/Install/ubunt_16_04/2.python3/install_script.sh
@@ -0,0 +1,6 @@
+#! /bin/bash 
+
+
+wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86.sh
+bash Anaconda3-2018.12-Linux-x86.sh
+gnome-terminal jupyter notebook
--- diary/Install/ubunt_18_04/8.virtualBox/install_scripts.sh
+++ diary/Install/ubunt_18_04/8.virtualBox/install_scripts.sh
@@ -3,4 +3,14 @@
 wget http://mirror.kakao.com/ubuntu-releases/16.04.7/ubuntu-16.04.6-desktop-i386.iso
 wget https://releases.ubuntu-mate.org/archived/18.04/ubuntu-mate-18.04.5-desktop-i386.iso
 
+##########################################
+#  unetbootin 
+##########################################
+sudo add-apt-repository ppa:gezakovacs/ppa
+sudo apt-get update
+sudo apt-get install unetbootin
+
+##########################################
+#  virtualbox 
+##########################################
 sudo apt install virtualbox
