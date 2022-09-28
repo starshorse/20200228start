fed7adcf2e6e0ab4c38d294ee4ad3a2151fa8429
DESKTOP-JC987LD\user <star_horse@naver.com>
2022-09-28 09:42:29 +0900

    local ubuntu 18.04 64bit

--- diary/Install/ubunt_18_04/8.virtualBox/install_scripts.sh
+++ diary/Install/ubunt_18_04/8.virtualBox/install_scripts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
+wget https://mirror.kakao.com/ubuntu-releases/bionic/ubuntu-18.04.6-desktop-amd64.iso
 wget http://mirror.kakao.com/ubuntu-releases/16.04.7/ubuntu-16.04.6-desktop-i386.iso
 wget https://releases.ubuntu-mate.org/archived/18.04/ubuntu-mate-18.04.5-desktop-i386.iso
 
