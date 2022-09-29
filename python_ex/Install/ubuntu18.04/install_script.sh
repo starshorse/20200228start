fbcac903eea1a23143d45cf5c59e8e8f55fbe6c3
DESKTOP-JC987LD\user <star_horse@naver.com>
2022-09-29 14:45:27 +0900

    pyhton_ex mysql, slack

---
+++ python_ex/Install/ubuntu18.04/install_script.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+
+###############################################################
+# slack  installation 
+###############################################################
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
+sudo apt install ./slack-desktop-*.deb
+
+###############################################################
+#  mysql installation 
+###############################################################
+
+sudo apt-get update 
+sudo apt-get install mysql-server -y
+sudo systemctl start mysql 
+sudo systemctl enable mysql 
+sudo mysql -u root -p < mysql_script.sql  
+
+echo "SHOW VARIABLES LIKE '%version%';"  
+
+
+###############################################################
+#  npm installation 
+###############################################################
+
+cd ../../
+npm init -y
+npm install grunt-cli grunt -g  
+npm install grunt-contrib-connect grunt --save-dev 
+cp ./Install/ubuntu18.04/Gruntfile.js  .
+mkdir www-root  
+cp ./install/ubuntu18.04/index.html ./www-root 
+grunt connect 
+
+
+###############################################################
+#  Django installation 
+###############################################################
+conda install django 
+django-admin startproject mysite 
+cd mysite
+python manage.py runserver 
+
+
+
+
+
