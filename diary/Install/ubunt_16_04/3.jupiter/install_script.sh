d7813b252567d3097ee340a6a818b844efe2c8a7
starshorse <star_horse@naver.com>
2022-09-30 18:13:25 +0900

    home-DT

---
+++ diary/Install/ubunt_16_04/3.jupiter/install_script.sh
@@ -0,0 +1,14 @@
+sudo apt install build-essential -y 
+
+DIR="$( cd "$( dirname "$0" )" && pwd -P )"
+echo $DIR
+
+cp make_setenv.sh  /home/rrr/workdir/gitdn/20200228start/diary/Install/ezoffice_builder/Bld_framework/
+cd /home/rrr/workdir/gitdn/20200228start/diary/Install/ezoffice_builder/Gen_framework/sys02_linux/
+bash build_exe.sh 
+cp ./bin_linux/* /home/rrr/workdir/gitdn/20200228start/diary/Install/ezoffice_builder/Bld_framework/bin/
+cd /home/rrr/workdir/gitdn/20200228start/diary/Install/ezoffice_builder/Bld_framework/ 
+chmod +777 *.sh && chmod +777 ./bin/* 
+./make_setenv.sh
+
+
---
+++ diary/Install/ubunt_16_04/3.jupiter/make_setenv.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -i  
+input=$1
+
+sudo apt-get install -y git 
+sudo apt-get remove vim-tiny 
+sudo apt-get install vim-gnome 
+sudo apt-get install vim-gtk3 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+
+cd ../Gen_framework/sys02_linux/ 
+./build_exe.sh 
+cp ./bin_linux/*  ../../Bld_framework/bin 
+cd ../../Bld_framework
+
+PATH_C=$(pwd) 
+echo "export PATH=\"$PATH_C/bin:\$PATH\"" >> ~/.bashrc
+source ~/.bashrc
+. ~/.bashrc
+
+if [ $input = "1" ]
+then
+    make_e_vimrcexe
+else
+    make_e_vimrc2exe
+fi 
+mv .vimrc ~/ 
+vim +PluginInstall +qall 
+
+sudo apt-get install -y tmux 
+sudo apt-get install -y curl
+#sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+#
+#source ~/.bashrc
+#. ~/.bashrc
+#
+#nvm install 12.22.1
+#nvm use 12.22.1 
