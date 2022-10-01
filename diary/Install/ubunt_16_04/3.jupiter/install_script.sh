23ff5d4794bc1c29e648fc0adbfa8d878b13ce0e
starshorse <star_horse@naver.com>
2022-10-01 11:07:12 +0900

    home-DT

--- diary/Install/ubunt_16_04/3.jupiter/install_script.sh
+++ diary/Install/ubunt_16_04/3.jupiter/install_script.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 echo $DIR
 
 cp make_setenv.sh  /home/rrr/workdir/gitdn/20200228start/diary/Install/ezoffice_builder/Bld_framework/
+cp make_ez.sh  /home/rrr/workdir/gitdn/20200228start/diary/Install/ezoffice_builder/Bld_framework/
 cd /home/rrr/workdir/gitdn/20200228start/diary/Install/ezoffice_builder/Gen_framework/sys02_linux/
 bash build_exe.sh 
 cp ./bin_linux/* /home/rrr/workdir/gitdn/20200228start/diary/Install/ezoffice_builder/Bld_framework/bin/
