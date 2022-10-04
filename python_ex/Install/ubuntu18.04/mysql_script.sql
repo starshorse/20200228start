89299b1e1f23ce79c648c26d0ee82526f5c1794c
starshorse <star_horse@naver.com>
2022-10-04 12:55:21 +0900

    local python_ex /Install mysql_script

--- python_ex/Install/ubuntu18.04/mysql_script.sql
+++ python_ex/Install/ubuntu18.04/mysql_script.sql
@@ -1,11 +1,18 @@
 SHOW VARIABLES LIKE '%version%';  
 USE mysql;
+
+delete from mysql.user where User='starshorse' ;
+delete from mysql.db where User='starshorse' ;
+drop database starshorse; 
+flush privileges ;
+
 create user starshorse ;
 create user starshorse@localhost identified by '1234';
--- create user starshorse@'%' identified by '1234';
-grant all privileges on *.* to starshorse@localhost idnetified by '1234';
+-- create user starshorse@'%';
+grant all privileges on *.* to starshorse@'localhost';
 flush privileges;
 show grants for starshorse@localhost;
+create database starshorse; 
 SELECT User, Host, plugin FROM mysql.user;
 select user , host, plugin from user; 
-exit;
+quit
