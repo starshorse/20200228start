c93277086c2b7e04a8c3f7117f5c5fcca19172c0
DESKTOP-JC987LD\user <star_horse@naver.com>
2022-10-05 17:26:10 +0900

    local ezch_task_app tbl_editor

--- diary/Install/ezoffice_builder/Bld_framework/codeTree/ezworks/app/apps/ezch_task_app/app.js
+++ diary/Install/ezoffice_builder/Bld_framework/codeTree/ezworks/app/apps/ezch_task_app/app.js
@@ -1,5 +1,6 @@
 angular.module('ezch_task_app',[
-	'spreadjs'
+	'spreadjs',
+	'ezch_tbl_editor_app'
 ])
 .controller('ezch_task_appCtrl',['$scope',function( $scope ){
 }])
@@ -8,12 +9,15 @@ angular.module('spreadjs',[])
 .controller('spreadjsCtrl',[
 '$scope', 
 'spreadjs_service',	
-	function(
+async function(
 		$scope,
 		spreadjs_service
 	){
 		var spread = new GC.Spread.Sheets.Workbook( document.getElementById('ss'), { sheetCount: 3 }) 
-		spreadjs_service.initSpread( spread ) 
+	    await spreadjs_service.initSpread( spread ) 
+		spread.bind( GC.Spread.Sheets.Events.ButtonClicked, ( sender, args )=>{
+			spreadjs_service.spread_buttonClicked( sender , args ) 
+		})
 }])
 .factory('spreadjs_factory', [function(){
 	var spreadjs_factory ={
@@ -27,15 +31,74 @@ angular.module('spreadjs',[])
 .service('spreadjs_service',[
 'spreadjs_factory',
 '$http',	
+'ezch_tbl_editor_appService',	
 	function( 
 		spreadjs_factory ,
-		$http
+		$http,
+		ezch_tbl_editor_appService
 	){
+		this.spread_buttonClicked = ( sender, args)=>{
+			let spread = spreadjs_factory.spread 
+			let sheet0 = spread.getSheet(0)
+			let cell_lock = sheet0.getRange('D2:D2')
+			let sheet_name = args.sheet.name() 
+			switch( sheet_name ){
+				case 'Sheet1':
+					 if( args.row == cell_lock.row )console.log( sheet0.getSelections() ) 
+					break;
+				case 'Sheet2':
+//					if( args.row == cell_add.row )ezch_tbl_editor_appFactory.update_schema_table( spread ) // only add update. 
+					break;
+			   default:	
+			}
+		}
 		this.initSpread = async ( spread )=>{
+			spreadjs_factory.spread = spread 
 			let sheet0 = spread.getSheet(0) 
+			let sheet1 = spread.getSheet(1) 
+/*			
 			let response = await $http.get('/Hermes/ezchemtech/TableEditor/Data/정기업무기본정보') 
             spreadjs_factory.DbData = response.data.tbl_data 
 			sheet0.setDataSource( spreadjs_factory.DbData ) 
+*/
+			sheet0.tables.add('TableData', 8,1,150,4) 
+			sheet1.tables.add('TableSchema', 4,2,150,10) 
+			let tbl_name ='정기업무기본정보'
+			sheet0.addColumns( 2, 10) 
+
+            sheet0.frozenRowCount(9)  
+			let cell_buttons = sheet0.getRange('B2:B2') 
+			sheet0.addSpan( cell_buttons.row, cell_buttons.col , 1, 2) 
+			let cellType_button1 = new GC.Spread.Sheets.CellTypes.Button() 
+			cellType_button1.text('대량삽입') 
+		    sheet0.getCell( cell_buttons.row, cell_buttons.col ).backColor('#EEEEEE').cellType( cellType_button1 ) 
+
+			sheet0.addSpan( cell_buttons.row, cell_buttons.col+2 , 1, 2) 
+			let cellType_button2 = new GC.Spread.Sheets.CellTypes.Button() 
+			cellType_button2.text('잠금해제') 
+		    sheet0.getCell( cell_buttons.row, cell_buttons.col +2 ).backColor('#EEEEEE').cellType( cellType_button2 ) 
+
+			sheet0.addSpan( cell_buttons.row, cell_buttons.col+4 , 1, 2) 
+			let cellType_button3 = new GC.Spread.Sheets.CellTypes.Button() 
+			cellType_button3.text('업데이트') 
+		    sheet0.getCell( cell_buttons.row, cell_buttons.col +4 ).backColor('#EEEEEE').cellType( cellType_button3 ) 
+
+			sheet0.setRowHeight( cell_buttons.row, 30 )
+
+
+			let cell_mass = sheet0.getRange('B3:B3')
+			sheet0.getRange( cell_mass.row , cell_mass.col , 1 , 100 ).borderTop( new GC.Spread.Sheets.LineBorder('#777777', GC.Spread.Sheets.LineStyle.medium ))
+			sheet0.getRange( cell_mass.row , cell_mass.col , 5 , 1 ).borderLeft( new GC.Spread.Sheets.LineBorder('#777777', GC.Spread.Sheets.LineStyle.medium ))
+			sheet0.getRange( cell_mass.row + 4 , cell_mass.col , 1 , 100 ).borderBottom( new GC.Spread.Sheets.LineBorder('#777777', GC.Spread.Sheets.LineStyle.medium ))
+
+			await ezch_tbl_editor_appService.update_schema_table( spread , tbl_name ) 
+			await ezch_tbl_editor_appService.update_data_table( spread , tbl_name ) 
+			
+			sheet0.options.isProtected = false 
+			sheet0.getRange('B3:AD9').locked( false )
+			sheet0.options.isProtected = true 
+			sheet0.options.protectionOptions.allowFilter = true  
+
 		}
 	}
 ])
