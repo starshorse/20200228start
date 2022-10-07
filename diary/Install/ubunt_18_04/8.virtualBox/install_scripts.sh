917e679e29b6cca88d31f5f5bccf79252dd737d8
starshorse <star_horse@naver.com>
2022-10-07 17:17:57 +0900

    local virtualbox ubuntu  22.04.1LTS

--- diary/Install/ubunt_18_04/8.virtualBox/install_scripts.sh
+++ diary/Install/ubunt_18_04/8.virtualBox/install_scripts.sh
@@ -3,7 +3,7 @@
 wget https://mirror.kakao.com/ubuntu-releases/bionic/ubuntu-18.04.6-desktop-amd64.iso
 wget http://mirror.kakao.com/ubuntu-releases/16.04.7/ubuntu-16.04.6-desktop-i386.iso
 wget https://releases.ubuntu-mate.org/archived/18.04/ubuntu-mate-18.04.5-desktop-i386.iso
-
+wget https://mirror.kakao.com/ubuntu-releases/jammy/ubuntu-22.04.1-desktop-amd64.iso
 ##########################################
 #  unetbootin 
 ##########################################
