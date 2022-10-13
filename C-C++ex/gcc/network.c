09ba3e1651885412bc95ae8ae2416803e852a2aa
DESKTOP-JC987LD\user <star_horse@naver.com>
2022-10-13 09:34:37 +0900

    local C=C++ex

--- C-C++ex/gcc/network.c
+++ C-C++ex/gcc/network.c
@@ -6,11 +6,16 @@
 	#include <sys/socket.h>
         #include <netinet/in.h> 
 #endif 
+#include <string.h> 
 
 int main( int argc , char** argv )
 {
 	int serv_sock;
 	struct sockaddr_in serv_addr; 
 	serv_sock = socket( PF_INET,  SOCK_STREAM , 0);
+	memset( &serv_addr, 0 , sizeof( serv_addr )); 
+	serv_addr.sin_family = AF_INET ; 
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY) ;
+	serv_addr.sin_port = htons( atoi( argv[1] );
 	return 0 ;
 }
