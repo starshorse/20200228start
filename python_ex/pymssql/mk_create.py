c0f07ac8b870f848ff5b0494dc97632365b92f64
DESKTOP-JC987LD\user <star_horse@naver.com>
2022-10-28 17:07:53 +0900

    local dirary/Install/windows 10/4.WSL

--- python_ex/pymssql/mk_create.py
+++ python_ex/pymssql/mk_create.py
@@ -1,12 +1,20 @@
 import pymssql 
 import jwt 
 import pandas as pd 
+import pdb 
 
 ezchemtech_ml_key = { company:'ezchemtech' , source:'excel', id:'richard.choi@ez-office.co.kr' , type: 'Machine' } 
 HADES_1_KEY = 'ez-office.co.kr'
 
-encode_jwt =  jwt.encode( ezchemtech_ml_key , HADES_1_KEY ) 
+encode_jwt =  jwt.encode( ezchemtech_ml_key , HADES_1_KEY, 'HS256' ) 
 print('encode key', encode_jwt ) 
 
-decode_ml_key = jwt.decode( encode_jwt , HADES_1_KEY )
+decode_ml_key = jwt.decode( encode_jwt , HADES_1_KEY, 'HS256' )
 print( ezchemtech_ml_key , decode_ml_key ) 
+
+conn1 = pymssql.connect( server='' , database='' , user='' , password ='' ) 
+sql = 'select * from dbo.TB_Auth_Macine' 
+df1 = pd.read_sql( sql, con=conn1 ) 
+
+
+pdb.set_trace() 
