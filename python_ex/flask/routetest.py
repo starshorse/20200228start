1c5570b28749730bbacda1c90f24bc1bff9aa621
starshorse <star_horse@naver.com>
2022-10-31 18:00:08 +0900

    local python pandas mssql

---
+++ python_ex/flask/routetest.py
@@ -0,0 +1,15 @@
+from flask import Flask
+try:
+    from urllib.parse import unquote  # PY3
+except ImportError:
+    from urllib import unquote  # PY2
+
+app = Flask(__name__)
+
+@app.route('/v1/<path:param>')  # NOTE: <path:param> is required to match /
+def f(param=''):
+    return (
+        f"param: {param}\ndecoded param: {unquote(param)}\n",
+        200,
+        {'content-type': 'text/plain'}
+    )
