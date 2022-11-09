2d29d586ca047a2a5fc791592aa7600ba7504c3e
DESKTOP-JC987LD\user <star_horse@naver.com>
2022-11-09 15:44:26 +0900

    local goomide test

--- server/app.js
+++ server/app.js
@@ -14,10 +14,10 @@ var express = require('express');
 var config = require('./config/environment');
 // Setup server
 var app = express();
-var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
-// app.use('/', express.static( __dirname +'/client')); 
+app.use('/', express.static( __dirname +'/client')); 
+var server = require('http').createServer(app);
 
 // Start server
 server.listen(config.port, config.ip, function () {
