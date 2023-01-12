0e625e4adb32211863b8ed4f76f5712bc7aca100
starshorse <star_horse@naver.com>
2023-01-12 18:12:30 +0900

    local ezworks log monitor

--- ezworks/client/app/ezof_log_monitor/app.js
+++ ezworks/client/app/ezof_log_monitor/app.js
@@ -5,4 +5,23 @@ angular.module('ezof_log_monitor',[
 	'ezof_log_monitorService'
 ])
 .controller('ezof_log_monitorCtrl', ['$scope','$injector', function( $scope, $injector ){
+	var ezof_log_monitorFactory = $injector.get('ezof_log_monitorFactory')
+	var ezof_log_monitorService = $injector.get('ezof_log_monitorService')
+	$scope.user_DB
+	$scope.alert_info_message = { class: 'warning' ,message: 'init' }
+	const updateUserDB = ( user_db )=>{
+		$scope.user_DB = user_db;
+		if( !$scope.$$phase )$scope.$apply() 
+	}
+	const updateAlertInfo = ( alert_info_message )=>{
+		$scope.alert_info_message = alert_info_message; 	
+		if( !$scope.$$phase )$scope.$apply() 
+	}
+	ezof_log_monitorFactory.update_cur_db = updateUserDB ;	
+	ezof_log_monitorFactory.updateAlertInfo = updateAlertInfo ; 
+	document.getElementById('inputGroupSelect01').addEventListener('change', function(e){
+		let  db_name = document.getElementById('inputGroupSelect01').value 
+		let  spread = ezof_log_monitorFactory.spread ; 
+                ezof_log_monitorService.updateTblData( spread, db_name );
+	})
 }])
--- ezworks/client/service/spreadjs/custom/ezof_log_monitor/index.js
+++ ezworks/client/service/spreadjs/custom/ezof_log_monitor/index.js
@@ -1,12 +1,16 @@
 angular.module('ezof_log_monitorService',[])
 .factory('ezof_log_monitorFactory', ['$injector',function($injector){
 	var ezof_log_monitorFactory = {
+		spread: null , 
 		tblView_tbl : { tbl_pos: null ,  tbl_view : null , tbl_columns: null , tbl_data_1 : null } ,
 		cur_db: 'ezchemtech',
 		tbl_name:'TB_Log_Collector',
 		cur_id: 'richard.choi@ez-office.co.kr',
 //functions.		
-		update_editLists : null 		
+		update_editLists : null,  		
+		update_cur_db: null ,
+		updateAlertInfo: null 
+
 	}	
 	return ezof_log_monitorFactory 
 }])
@@ -14,56 +18,67 @@ angular.module('ezof_log_monitorService',[])
 	var ezof_log_monitorFactory = $injector.get('ezof_log_monitorFactory') 
 	var $http = $injector.get('$http') 
 	this.updateTblData = async ( spread , db_name )=>{
+	      sheet0 = spread.getSheet(0); 	
 	      let  tbl_name = ezof_log_monitorFactory.tbl_name ;
 	      let  tbl_view = ezof_log_monitorFactory.tblView_tbl.tbl_view ;
 	      let  hades_data = await $http.get(`/log_monitor/${ db_name }`)
-	      ezof_log_monitorFactory.tblView_tbl.tbl_data_1 = hades_data.data.DATA 	
+	      ezof_log_monitorFactory.tblView_tbl.tbl_data_1 = hades_data.data.DATA.sort((a,b)=>b.seq-a.seq) 	
 	      let  tbl_info = ezof_log_monitorFactory.tblView_tbl ; 	
+	      if( sheet0.getValue( 1, 2 ))tbl_info.tbl_data_1 = tbl_info.tbl_data_1.filter(( ent )=>ent.hostname == 'Jupitor' ) 	
+	      let last7days = (new Date()).setDate((new Date()).getDate() - 7 )	
+	      let last7days_count = tbl_info.tbl_data_1.filter((ent)=>{ return ( (new Date(ent.RegDate)).getTime() > last7days )}).length ; 	
+	       console.log( last7days_count )	
+	      let cell_D3  = sheet0.getRange('D3')
+	      sheet0.getCell( cell_D3.row, cell_D3.col ).value(last7days_count); 	
+
 	      tbl_view.bind( null, 'tbl_data_1', tbl_info ) 	
+	      ezof_log_monitorFactory.update_cur_db( db_name ); 
 		
 	}
 	this.initTblView = async ( spread )=>{
+	      ezof_log_monitorFactory.spread = spread ; 
 	      let sheet0 = spread.getSheet(0); 
 	      sheet0.name('TblView')
 	      sheet0.setColumnCount(30)
 	      sheet0.setRowCount( 40000 ) 	
 	      let defaultStyle = new GC.Spread.Sheets.Style() 
-	      sheet0.suspendPaint();
 	      sheet0.setDefaultStyle( defaultStyle ) 
-	      sheet0.resumePaint() 
-	      sheet0.frozenRowCount(17) 
-	      let cell_massCheck = sheet0.getRange('C7') 
-	      sheet0.setRowHeight( cell_massCheck.row-1, 40 ) 	
-	      sheet0.setRowHeight( cell_massCheck.row, 40 ) 	
-	      sheet0.setValue( cell_massCheck.row-1 , cell_massCheck.col -1 , 'sql조건' ) 
-	      sheet0.setValue( cell_massCheck.row , cell_massCheck.col -1 , '대량삽입' ) 
-	      let cell_c1 = new GC.Spread.Sheets.CellTypes.CheckBox() 
-	      sheet0.clear( cell_massCheck, cell_massCheck.col, 1,1, GC.Spread.Sheets.SheetArea.viewport, GC.Spread.Sheets.StorageType.style ) 
-	      sheet0.setCellType( cell_massCheck.row, cell_massCheck.col, cell_c1 ) 	
-	      sheet0.setCellType( cell_massCheck.row-1, cell_massCheck.col, cell_c1 ) 	
-		
-	      sheet0.setValue( cell_massCheck.row-1 , cell_massCheck.col +3 , '현재테이블' ) 
-	      let cell_bt1 = new GC.Spread.Sheets.CellTypes.Button() 
-	      cell_bt1.text('실행')
-	      sheet0.setCellType( cell_massCheck.row , cell_massCheck.col +1 , cell_bt1 ) 
-	      sheet0.setCellType( cell_massCheck.row-1 , cell_massCheck.col +1 , cell_bt1 ) 
-	      let cell_bt2 = new GC.Spread.Sheets.CellTypes.Button() 
-	      cell_bt2.text('잠금해제')
-	      sheet0.setCellType( cell_massCheck.row , cell_massCheck.col +4 , cell_bt2 ) 
-	      let cell_bt3 = new GC.Spread.Sheets.CellTypes.Button() 
-	      cell_bt3.text('업데이트')
-	      sheet0.setCellType( cell_massCheck.row , cell_massCheck.col +5 , cell_bt3 ) 
+	      sheet0.frozenRowCount(7) 
+	      sheet0.setColumnWidth( 2, 800 )	
+	      sheet0.setColumnWidth( 4, 500 )	
+	      sheet0.setColumnWidth( 8, 300 )	
 
-	      sheet0.addSpan( cell_massCheck.row - 3 , cell_massCheck.col +6 , 4, 4 )			
-	      sheet0.addSpan( cell_massCheck.row - 3 , cell_massCheck.col +10 , 4, 4 )			
-	 //     ezof_log_monitorFactory.sql_state.pos = sheet0.getRange('I4')	
-		
+	      let cell_jupiterCheck = new GC.Spread.Sheets.CellTypes.CheckBox() 
+	      cell_jupiterCheck.caption("Jupitor 만보기")	
+	      sheet0.getCell(1,2).cellType( cell_jupiterCheck ) 	
+	      sheet0.getCell(2,2).text("최근 1주일 Log 횟수:")		
 
-	      let cell_tblData = sheet0.getRange('B17') 	
+	      let cell_update_pos = sheet0.getRange('E2')
+	      let buttonCommandfunction = new GC.Spread.Sheets.Style() ;
+	      buttonCommandfunction.cellButtons =
+	      [
+		      {
+			  caption: "Update",
+			  buttonBackColor: "#00C2D6",    
+			  command:( sheet, row , col, option )=>
+			  {
+				this.updateTblData( spread, ezof_log_monitorFactory.cur_db ); 
+			  } 	  
+
+		       } 	      
+	       ];
+	       sheet0.setStyle( cell_update_pos.row, cell_update_pos.col , buttonCommandfunction ); 	 	
+
+	      let cell_tblData = sheet0.getRange('B7') 	
+	      sheet0.suspendPaint();
 	      let table1 = ezof_log_monitorFactory.tblView_tbl.tbl_view = sheet0.tables.add('tableView', cell_tblData.row , cell_tblData.col, 120, 20 );
 	      ezof_log_monitorFactory.tblView_tbl.tbl_pos = cell_tblData 
 	      table1.style( GC.Spread.Sheets.Tables.TableThemes['medium4']) 
 	      await this.updateTblData( spread , ezof_log_monitorFactory.cur_db ) 	
+	      setInterval( this.updateTblData , 60*60*1000, spread, ezof_log_monitorFactory.cur_db ) 	
+	      ezof_log_monitorFactory.updateAlertInfo({ class:'success', message:'setInterval Started'});
+	      sheet0.resumePaint() 
+
 	}
 }])
 .service('ezof_log_monitor_eventsService', ['$injector', function($injector){
