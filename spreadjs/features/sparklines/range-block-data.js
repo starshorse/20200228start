f7d859d0153bca5ff168974618445d4876d55c29
starshorse <star_horse@naver.com>
2023-03-09 14:05:27 +0900

    local spreadjs sparkline

---
+++ spreadjs/features/sparklines/range-block-data.js
@@ -0,0 +1,343 @@
+var dataJson = {
+      "name": "DataSource",
+      "rowCount": 192,
+      "columnCount": 17,
+      "activeRow": 7,
+      "activeCol": 11,
+      "theme": "Office",
+      "data": {
+        "dataTable": {
+          "0": {
+            "0": {
+              "value": "Rank"
+            },
+            "1": {
+              "value": "Name"
+            },
+            "2": {
+              "value": "Image"
+            },
+            "3": {
+              "value": "Price"
+            },
+            "4": {
+              "value": "Customers"
+            },
+            "5": {
+              "value": "Facebook Sales"
+            },
+            "6": {
+              "value": "Instagram Sales"
+            },
+            "7": {
+              "value": "Amazon Sales"
+            },
+            "8": {
+              "value": "Total Units"
+            },
+            "9": {
+              "value": "Target"
+            }
+          },
+          "1": {
+            "0": {
+              "value": 1
+            },
+            "1": {
+              "value": "The Runner"
+            },
+            "2": {
+              "value": "the_runner.png"
+            },
+            "3": {
+              "value": 39.99
+            },
+            "4": {
+              "value": 2630
+            },
+            "5": {
+              "value": 229
+            },
+            "6": {
+              "value": 1462
+            },
+            "7": {
+              "value": 1230
+            },
+            "8": {
+              "value": 3500
+            },
+            "9": {
+              "value": 2500
+            }
+          },
+          "2": {
+            "0": {
+              "value": 2
+            },
+            "1": {
+              "value": "The Boxer"
+            },
+            "2": {
+              "value": "the_boxer.png"
+            },
+            "3": {
+              "value": 34.99
+            },
+            "4": {
+              "value": 2365
+            },
+            "5": {
+              "value": 476
+            },
+            "6": {
+              "value": 789
+            },
+            "7": {
+              "value": 1256
+            },
+            "8": {
+              "value": 3500
+            },
+            "9": {
+              "value": 2000
+            }
+          },
+          "3": {
+            "0": {
+              "value": 3
+            },
+            "1": {
+              "value": "The Walker"
+            },
+            "2": {
+              "value": "the_walker.png"
+            },
+            "3": {
+              "value": 29.99
+            },
+            "4": {
+              "value": 1900
+            },
+            "5": {
+              "value": 856
+            },
+            "6": {
+              "value": 856
+            },
+            "7": {
+              "value": 592
+            },
+            "8": {
+              "value": 3500
+            },
+            "9": {
+              "value": 1850
+            }
+          },
+          "4": {
+            "0": {
+              "value": 4
+            },
+            "1": {
+              "value": "The Athlete"
+            },
+            "2": {
+              "value": "the_athlete.png"
+            },
+            "3": {
+              "value": 59.99
+            },
+            "4": {
+              "value": 1632
+            },
+            "5": {
+              "value": 156
+            },
+            "6": {
+              "value": 968
+            },
+            "7": {
+              "value": 894
+            },
+            "8": {
+              "value": 3500
+            },
+            "9": {
+              "value": 1500
+            }
+          },
+          "5": {
+            "0": {
+              "value": 5
+            },
+            "1": {
+              "value": "The Topper"
+            },
+            "2": {
+              "value": "the_topper.png"
+            },
+            "3": {
+              "value": 49.99
+            },
+            "4": {
+              "value": 1589
+            },
+            "5": {
+              "value": 780
+            },
+            "6": {
+              "value": 94
+            },
+            "7": {
+              "value": 1100
+            },
+            "8": {
+              "value": 3500
+            },
+            "9": {
+              "value": 1500
+            }
+          }
+        },
+        "defaultDataNode": {
+          "style": {
+            "themeFont": "Body"
+          }
+        }
+      },
+      "rowHeaderData": {
+        "defaultDataNode": {
+          "style": {
+            "themeFont": "Body"
+          }
+        }
+      },
+      "colHeaderData": {
+        "defaultDataNode": {
+          "style": {
+            "themeFont": "Body"
+          }
+        }
+      },
+      "columns": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        {
+          "size": 101
+        },
+        {
+          "size": 93
+        }
+      ],
+      "leftCellIndex": 0,
+      "topCellIndex": 0,
+      "selections": {
+        "0": {
+          "row": 7,
+          "rowCount": 1,
+          "col": 11,
+          "colCount": 1
+        },
+        "length": 1
+      },
+      "rowOutlines": {
+        "items": []
+      },
+      "columnOutlines": {
+        "items": []
+      },
+      "cellStates": {},
+      "outlineColumnOptions": {},
+      "autoMergeRangeInfos": [],
+      "printInfo": {
+        "paperSize": {
+          "width": 850,
+          "height": 1100,
+          "kind": 1
+        }
+      },
+      "tables": [
+        {
+          "name": "product_table",
+          "row": 0,
+          "col": 0,
+          "rowCount": 6,
+          "colCount": 10,
+          "showResizeHandle": true,
+          "style": {
+            "buildInName": "Medium11"
+          },
+          "rowFilter": {
+            "range": {
+              "row": 1,
+              "rowCount": 5,
+              "col": 0,
+              "colCount": 10
+            },
+            "typeName": "HideRowFilter",
+            "dialogVisibleInfo": {},
+            "filterButtonVisibleInfo": {
+              "0": true,
+              "1": true,
+              "2": true,
+              "3": true,
+              "4": true,
+              "5": true,
+              "6": true,
+              "7": true,
+              "8": true,
+              "9": true
+            },
+            "showFilterButton": true
+          },
+          "columns": [
+            {
+              "id": 1,
+              "name": "Rank"
+            },
+            {
+              "id": 2,
+              "name": "Name"
+            },
+            {
+              "id": 3,
+              "name": "Image"
+            },
+            {
+              "id": 4,
+              "name": "Price"
+            },
+            {
+              "id": 5,
+              "name": "Customers"
+            },
+            {
+              "id": 6,
+              "name": "Facebook Sales"
+            },
+            {
+              "id": 7,
+              "name": "Instagram Sales"
+            },
+            {
+              "id": 8,
+              "name": "Amazon Sales"
+            },
+            {
+              "id": 9,
+              "name": "Total Units"
+            },
+            {
+              "id": 10,
+              "name": "Target"
+            }
+          ]
+        }
+      ]
+    }
