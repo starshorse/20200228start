0d5b99efd92115bd18d4432ef448b15d18b3612b
DESKTOP-JC987LD\user <star_horse@naver.com>
2023-03-13 15:44:09 +0900

    local arduCAM up

---
+++ diary/Install/raspberrian/arducam/digital_zoom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# W:Zoom in
+# S:Zoom out 
+# I:Move upward
+# K:Move downward 
+# J:Move left
+# L:Move right 
+# R:Reset
+# M: 10xZoom 
+
+libcamera-still -t 0 --viewfinder-width 2312 --viewfinder-height 1736 -k 
--- diary/Install/raspberrian/arducam/focusControl.sh
+++ diary/Install/raspberrian/arducam/focusControl.sh
@@ -1,5 +1,5 @@
-#!\bin\bash
+#!/bin/bash
 
 # Press the Up/Down Arrow for focus adjustment , press "Ctrl+C" to save or "r" to reset 
 cd Arducam-Pivariety-V4L2-Driver/focus
-python3 FocuserExample.py -d /dev/v4l-subev1 
+python3 FocuserExample.py -d /dev/v4l-subdev1 --focus-step 10  
