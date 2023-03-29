33ff402578546bde4e7d6e222bd5e00e380c15d7
starshorse <star_horse@naver.com>
2023-03-29 13:14:32 +0900

    local sqlserver sqlserverturial.net basics

---
+++ sqlServer/sqlservertutorial.net/basics/Section 03.Limiting rows/OFFSET_FETCH.sql
@@ -0,0 +1,38 @@
+SELECT
+    product_name,
+    list_price
+FROM
+    "production.products"
+ORDER BY
+    list_price,
+    product_name;
+GO
+/*
+To skip the first 10 products and return the rest, you use the OFFSET clause as shown in the following statement:
+*/
+SELECT
+    product_name,
+    list_price
+FROM
+    "production.products"
+ORDER BY
+    list_price,
+    product_name 
+OFFSET 10 ROWS;
+/*
+To skip the first 10 products and select the next 10 products, you use both OFFSET and FETCH clauses as follows:
+*/
+GO
+SELECT
+    product_name,
+    list_price
+FROM
+    "production.products"
+ORDER BY
+    list_price,
+    product_name 
+OFFSET 10 ROWS 
+FETCH NEXT 10 ROWS ONLY;
+/*
+To get the top 10 most expensive products you use both OFFSET and FETCH clauses:
+*/
