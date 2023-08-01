ce06428f2f50956a9ef9d60b34bb5129dd499858
starshorse <star_horse@naver.com>
2023-08-01 16:39:33 +0900

    local ezwork table_maker

--- ezworks/client/app/ezch_tbl_maker/app.js
+++ ezworks/client/app/ezch_tbl_maker/app.js
@@ -1,4 +1,5 @@
 angular.module('ezch_tbl_maker',[
+	'ngCookies',
 	'mySpreadjs',
 	'gc_spreadjs',
 //	'spreadjs_events',
@@ -7,7 +8,30 @@ angular.module('ezch_tbl_maker',[
 .controller('ezch_tbl_makerCtrl', ['$scope','$injector', function( $scope, $injector ){
 	var ezch_tbl_makerFactory = $injector.get('ezch_tbl_makerFactory')
 	var ezch_tbl_makerService = $injector.get('ezch_tbl_makerService')
+	var $cookies = $injector.get('$cookies') 
+	var $http = $injector.get('$http') 
+
+	let user_id = $cookies.get('user') 
+	let db_name = $cookies.get('user_DB') 
+	$scope.user_DBs    
 	$scope.user_DB
+
+	$http.get(`/Hades/dba_editor/db_login_web/${ db_name }/${ user_id }`).then( async (respnose)=>{
+		  if( respnose.data.STATUS == -1 ){
+			  alert( respnose.data.ERRORMESSAGE );
+			  return -1;
+		  }
+		  let login_id = respnose.data.DATA[0].login_id  
+		  let result  = await $http.get(`/Hades/db_administration/db_list/${ db_name }/${ login_id }`) 
+		  if( result.data.STATUS == -1 ){
+			  alert( result.data.ERRORMESSAGE );
+			  return -1; 
+		  }
+		  $scope.user_DBs =  result.data.DATA 
+		  $scope.user_DB =   db_name ; 
+		  $scope.$apply(); 
+	})
+
 	$scope.alert_info_message = { class: 'warning' ,message: 'init' }
 	const updateUserDB = ( user_db )=>{
 		$scope.user_DB = user_db;
