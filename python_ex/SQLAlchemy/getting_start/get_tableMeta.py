3cd44569205214a4a852642c51ee54fd9a9c430d
starshorse <star_horse@naver.com>
2024-02-14 07:51:55 +0900

    home python_ex/pandas to_sql

---
+++ python_ex/SQLAlchemy/getting_start/get_tableMeta.py
@@ -0,0 +1,20 @@
+import os 
+from sqlalchemy import create_engine,text 
+import sqlalchemy as db 
+import pandas as pd 
+from datetime import datetime 
+
+sqlserver_host = os.environ.get('SQLSERVER_HOST')
+sqlserver_id = os.environ.get('SQLSERVER_ID') 
+sqlserver_password = os.environ.get('SQLSERVER_PASSWORD') 
+
+print( sqlserver_host );
+engine = create_engine(f'mssql+pymssql://{ sqlserver_id }:{ sqlserver_password}@{ sqlserver_host }/ezoffice', echo=True)
+
+conn = engine.connect() 
+print( engine.table_names()) 
+
+metadata = db.MetaData() #extracting the metadata
+#account_list = db.Table(f'TB_{ db_name }_계좌목록_codeFApi', metadata, autoload=True, 
+#autoload_with=engine) #Table object
+print(repr(metadata.tables[f'TB_{ db_name }_계좌목록_codeFApi']))
---
+++ python_ex/examples[data-source]/pykrx/foreign_buy2DB.py
@@ -0,0 +1,59 @@
+from pykrx import stock
+import pandas as pd 
+from tabulate import tabulate 
+from dateutil.relativedelta import relativedelta 
+import datetime 
+from sqlalchemy import create_engine, text  
+
+# Create the engine to connect to the inbuilt  
+# sqllite database 
+engine = create_engine("sqlite+pysqlite:///:memory:") 
+
+now =  datetime.datetime.now() 
+before_one_day = now - relativedelta( days = 1 ) 
+before_one_week = now  - relativedelta( weeks = 1 )
+before_one_month = now - relativedelta( months = 1 )
+
+
+def load2DB( df ):
+    df.to_sql('load_data', engine ) 
+
+def showFromDB():
+    # establish the connection with the engine object
+    with engine.connect() as conn:
+        # let's select the column credit_history
+        # from the loan data table
+        result = conn.execute(text("SELECT * FROM load_data"))
+        # print the result
+        for row in result:
+            print(row)
+
+def tabulate_print( df ):
+    print(tabulate(  df , headers='keys', tablefmt='psql' )) 
+
+def get_df_period( before_period ):
+    df = stock.get_market_net_purchases_of_equities( before_period.strftime("%Y%m%d"), before_one_day.strftime("%Y%m%d"), "KOSPI", "외국인")
+    df = df.head(50)
+    df_1 = pd.read_json('../web_crawling/krx_kspi.json') 
+
+    df_LEFT = pd.merge( df, df_1 , left_on ='티커' , right_on='CODE', how='left')
+    df_LEFT = df_LEFT[ ( df_LEFT['PBR'] < 1) & (df_LEFT['PER'] < 20 ) ]
+    #df_LEFT =  df_LEFT[['종목명','PER','PBR','DIV']].sort_values(by='PBR').head(50) 
+    df_LEFT =  df_LEFT[['종목명','PER','PBR','DIV']].head(50) 
+    return df_LEFT 
+
+if __name__== "__main__":
+    df1 = get_df_period( before_one_week );
+    df2 = get_df_period( before_one_month ); 
+    print("weekly pics")
+    tabulate_print( df1 )
+    load2DB( df1 )
+    print("monthly pics")
+    tabulate_print( df2 )
+    df3 = pd.merge( df1, df2 , how='inner', on='종목명') 
+    df3 = df3[['종목명']]
+    print("Keeps")
+    tabulate_print( df3 )
+    print("FromDB")
+    showFromDB();
+
