3e99268ec0fbd7b77434df5848b4ba56cbee5dfc
starshorse <star_horse@naver.com>
2024-02-15 08:33:53 +0900

    home python socket.io

--- python_ex/flask/app.py
+++ python_ex/flask/app.py
@@ -1,10 +1,28 @@
+# 1. A minimal Application
+#   $flask --app hello run 
+# Externally Visible Server:
+#   $flask run --host=0.0.0.0 
+# Debug 
+#   $flask --app hello --debug run 
+# HTML Escaping.. 
+#   '<script>alert("bad")</script
+# set FLASK_APP=main.py
+#
+# 2. SocketIO
+#    pip install flask-socketio
+
 from flask import Flask 
+from flask_scoketio import SocketIO
 
 app = Flask( __name__ ) 
+app.config['SECRET_KEY'] = 'ez-office.co.kr' 
+socketio = SocketIO(app) 
+
 
 @app.route('/')
 def hello():
     return 'Hello, World'
 
 if __name__ == '__main__':
-    app.run()
+    #  app.run( port = 5000 )
+    socketio.run( app, debug=True )
