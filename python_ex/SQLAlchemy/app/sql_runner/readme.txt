c0d679dd3ff347b39fb603ce9450916ca887643f
starshorse <star_horse@naver.com>
2025-01-05 12:17:05 +0900

    home python_ex sqlalchemy

--- python_ex/SQLAlchemy/app/sql_runner/readme.txt
+++ python_ex/SQLAlchemy/app/sql_runner/readme.txt
@@ -1,3 +1,3 @@
-1. venv1\Script\activate  # sqlalchemy 1.w
+1. venv1\Script\activate  # sqlalchemy 1.4
 
 python main.py
--- python_ex/SQLAlchemy/getting_start/get_tableMeta.py
+++ python_ex/SQLAlchemy/getting_start/get_tableMeta.py
@@ -5,7 +5,7 @@
 
 """
 import os 
-from sqlalchemy import create_engine,text 
+from sqlalchemy import create_engine,text, select  
 import sqlalchemy as db 
 import pandas as pd 
 from datetime import datetime 
@@ -15,12 +15,13 @@ sqlserver_id = os.environ.get('SQLSERVER_ID')
 sqlserver_password = os.environ.get('SQLSERVER_PASSWORD') 
 
 print( sqlserver_host );
-engine = create_engine(f'mssql+pymssql://{ sqlserver_id }:{ sqlserver_password}@{ sqlserver_host }/ezoffice', echo=True)
+#engine = create_engine(f'mssql+pymssql://{ sqlserver_id }:{ sqlserver_password}@{ sqlserver_host }/ezoffice', echo=True)
+engine = create_engine('sqlite:///../alembic/test.db', echo=True );
 
 conn = engine.connect() 
 print( engine.table_names()) 
 
 metadata = db.MetaData() #extracting the metadata
-#account_list = db.Table(f'TB_{ db_name }_계좌목록_codeFApi', metadata, autoload=True, 
-#autoload_with=engine) #Table object
-print(repr(metadata.tables[f'TB_{ db_name }_계좌목록_codeFApi']))
+stables = db.Table('stables', metadata, autoload=True, autoload_with=engine) #Table object
+print( select( stables ));
+#print(repr(metadata.tables[f'TB_{ db_name }_계좌목록_codeFApi']))
