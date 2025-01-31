7ee345eb77124d8bacfbc1e160cce51d49742c6e
starshorse <star_horse@naver.com>
2025-02-01 07:32:55 +0900

    home remove ezworks dir

--- ezworks/client/lib/spreadjs/15.2.2/samples/ReactSample/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './Sample'
-import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2013white.css';
-
-const container = document.getElementById('root');
-
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
