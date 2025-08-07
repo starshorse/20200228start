13956500a3d294cb6cba3e43bcb1205fb57a75bf
DESKTOP-JC987LD\user <star_horse@naver.com>
2025-08-07 15:45:53 +0900

    local scss up

---
+++ bootstrap/theme/gulpfile.js
@@ -0,0 +1,92 @@
+var themes = [
+    "purple",
+    "purple.v5",
+    "office-white",
+    "office-white.v5",
+    "blazing-berry",
+    "blazing-berry.v5",
+    "blazing-dark",
+    "blazing-dark.v5",
+    "custom_1",
+    "custom_1.v5"
+];
+
+var srcDirName = "scss";
+var distCSS = (theme) => {
+    return theme.indexOf("v5") > -1 ? "dist.v5" : "dist";
+}
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
+    sourcemaps = require('gulp-sourcemaps'),
+    cleanCss = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    lazypipe = require('lazypipe'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    connect = require('gulp-connect');
+
+var compileTheme = lazypipe()
+    .pipe(sourcemaps.init)
+    .pipe(function () {
+        return sass().on('error', sass.logError);
+    })
+    .pipe(postcss, [autoprefixer({
+        browsers: [
+            'Chrome >= 35',
+            'Firefox >= 38',
+            'Edge >= 12',
+            'Explorer >= 10',
+            'iOS >= 8',
+            'Safari >= 8',
+            'Android 2.3',
+            'Android >= 4',
+            'Opera >= 12']
+    })
+    ])
+    .pipe(sourcemaps.write)
+    .pipe(rename, {basename: 'bootstrap'});
+var minifyCss = lazypipe()
+    .pipe(cleanCss)
+    .pipe(rename, {suffix: '.min'});
+
+themes.forEach(theme => {
+    gulp.task('compile-' + theme, (cb) => {
+        const themeDirName = theme.split('.')[0];
+        gulp.src(srcDirName + '/' + themeDirName + `/${theme}.scss`)
+            .pipe(compileTheme())
+            .pipe(gulp.dest(distCSS(theme) + '/' + themeDirName + '/'))
+            .pipe(minifyCss())
+            .pipe(gulp.dest(distCSS(theme) + '/' + themeDirName + '/'))
+            .pipe(connect.reload());
+        cb();
+    });
+    gulp.task('watch-' + theme, (cb) => {
+        const themeDirName = theme.split('.')[0];
+        gulp.watch(srcDirName + '/' + themeDirName + '/*.scss', gulp.series('compile-' + theme));
+        cb();
+    });
+});
+
+gulp.task('connect', (cb) => {
+    connect.server({
+        livereload: true,
+        port: 8888
+    });
+    cb();
+});
+
+gulp.task('html', (cb) => {
+    gulp.src('./*.html')
+        .pipe(connect.reload());
+    cb();
+});
+
+gulp.task('attachScss', gulp.series(themes.map(theme => "watch-" + theme)));
+gulp.task('attachHtml', (cb) => {
+    gulp.watch('./*.html', gulp.series('html'));
+    cb();
+});
+gulp.task('default', gulp.series(themes.map(theme => "compile-" + theme)));
+gulp.task('watch', gulp.series('default', 'connect', 'attachHtml', 'attachScss'));
+
---
+++ bootstrap/theme/script.js
@@ -0,0 +1,75 @@
+var dx = dx || {};
+
+(function () {
+    var currentScript = document.currentScript || (function () {
+        var scripts = document.getElementsByTagName('script');
+        return scripts[scripts.length - 1];
+    })();
+
+    var distDir = "dist";
+    if (currentScript.getAttribute("data-bs-version") === "5")
+        distDir += ".v5"
+
+    var themes = {};
+    themes["purple"] = {folder: "purple", title: "Purple"};
+    themes["office-white"] = {folder: "office-white", title: "Office White"};
+    themes["blazing-berry"] = {folder: "blazing-berry", title: "Blazing Berry"};
+    themes["blazing-dark"] = {folder: "blazing-dark", title: "Blazing Dark"};
+    themes["custom_1"] = {folder: "custom_1", title: "custom_1"};
+    themes["phoenix"] = {folder: "phoenix", title: "phoenix_1.9"};
+    themes["now-ui"] = {folder: "now-ui", title: "now-ui"};
+
+    $("a[href='#']").on("click", function (e) {
+        e.preventDefault();
+    });
+
+    function getCurrentThemeName() {
+        var url = window.location.href;
+        var regex = new RegExp('[?&]theme(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        var result = (!results || !results[2]) ? null : results[2];
+        return result || "purple";
+    }
+
+    function updateThemesList() {
+        var listHtml = "";
+        for (var themeName in themes) {
+            listHtml += "<a class='dropdown-item' href='javascript:;' onclick='dx.setTheme(&quot;" + themeName + "&quot;)'>" + themes[themeName].title + "</a>";
+        }
+        ;
+        $("#themesList").html(listHtml);
+    }
+
+    function updateCurrentTheme() {
+        var themeName = getCurrentThemeName();
+        var theme = themes[themeName];
+        if (theme) {
+            var minCssUrl = distDir + "/" + theme.folder + "/bootstrap.min.css";
+            var cssUrl = distDir + "/" + theme.folder + "/bootstrap.css";
+            $("#styleLink").attr("href", minCssUrl);
+            $("#downloadCss").attr("href", cssUrl);
+            $("#downloadMinCss").attr("href", minCssUrl);
+
+            $("#themeCaption").html(theme.title);
+            $("#downloadTheme").html(theme.title);
+        }
+    }
+
+    dx.setTheme = function (themeName) {
+        var anchor = document.location.href.split("#")[1];
+        window.history.pushState({themeName: themeName}, window.title, "?theme=" + themeName + (anchor ? "#" + anchor : ""));
+        updateCurrentTheme();
+    }
+    $(function () {
+        updateThemesList();
+        updateCurrentTheme();
+        $('body').scrollspy({target: '#scrollspy', offset: 90});
+        $('[data-toggle="popover"], [data-bs-toggle="popover').popover();
+        $('[data-toggle="tooltip"], [data-bs-toggle="tooltip"]').tooltip();
+        $('[data-toggle="indeterminate-checkbox"], [data-bs-toggle="indeterminate-checkbox"]').prop("indeterminate", true);
+    });
+    $(window).on('popstate', function () {
+        updateThemesList();
+        updateCurrentTheme();
+    });
+})();
